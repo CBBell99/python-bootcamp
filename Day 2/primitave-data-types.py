7c2cbbb53cfb3cc02d0abc5403692b24b9fda795
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-15 06:01:02 -0600

    example python data types

---
+++ Day 2/primitave-data-types.py
@@ -0,0 +1,20 @@
+#Data Types
+
+#Strings
+# get last character of string
+print("Hello"[4])
+
+#concatenate
+print("123" + "456")
+
+#Integer
+#calculate
+print(123 + 345)
+
+#Float
+3.14159
+
+#Boolean
+#first character must be capitalized
+True 
+False
