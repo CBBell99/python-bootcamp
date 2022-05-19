0846cb0facf8b793224f8752d3f0e34bf7a4d87b
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-18 21:54:41 -0700

    create logical opertor function

--- day03/pizza.py
+++ day03/pizza.py
@@ -1,4 +1,7 @@
 # 🚨 Don't change the code below 👇
+from re import S
+
+
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
@@ -8,16 +11,16 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # Write your code below this line 👇
 price = 0
 if size == "S":
-    price = 15
-    if add_pepperoni == "Y":
-        price += 2
+    price += 15
 elif size == "M":
-    price = 20
-    if add_pepperoni == "Y":
-        price += 3
+    price += 20
 else:
-    price = 25
-    if add_pepperoni == "Y":
+    price += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        price += 2
+    else:
         price += 3
 
 if extra_cheese == "Y":
