8cc8c0943e5dcfaeb69d387b17441166d6254277
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-21 05:53:32 -0700

    lesson on nested lists

--- day04/lists.py
+++ day04/lists.py
@@ -18,4 +18,18 @@ states_of_america.append("Belltron")
 # concatenate list
 states_of_america.extend(["hey", "yall"])
 
+print(len(states_of_america))
 print(states_of_america)
+
+# Will get error as index doesn"t exist
+print(states_of_america[7])
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries",  "Nectarines", "Apples",
+          "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale",  "Tomatoes", "Celery", "Potatoes"]
+
+# Nested list
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
