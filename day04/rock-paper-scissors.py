f0b7975fbda437274c02cdbedde1324274305b8f
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-22 05:57:28 -0600

    add images to game

--- day04/rock-paper-scissors.py
+++ day04/rock-paper-scissors.py
@@ -31,9 +31,11 @@ game_images = [rock, paper, scissors]
 
 user_choice = int(input(
     "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
 print(f"Computer chose {computer_choice}")
+print(game_images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number")
