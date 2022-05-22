2c4c18bf430891d71432d84d1fd25f11c049731f
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-22 05:29:36 -0600

    rps game

--- day04/rock-paper-scissors.py
+++ day04/rock-paper-scissors.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -28,5 +29,37 @@ scissors = '''
 # Write your code below this line 👇
 rps = [rock, paper, scissors]
 
-player = input(
-    "What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.")
+player_choice = input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+if player_choice == "0":
+    print(rock)
+elif player_choice == "1":
+    print(paper)
+elif player_choice == "2":
+    print(scissors)
+
+computer_choice = str(random.randint(0, 2))
+if computer_choice == "0":
+    print(rock)
+elif computer_choice == "1":
+    print(paper)
+elif computer_choice == "2":
+    print(scissors)
+
+print(computer_choice)
+
+if player_choice == computer_choice:
+    print("It's a draw")
+elif player_choice == "0" and computer_choice == "1":
+    print("You lose.")
+elif player_choice == "0" and computer_choice == "2":
+    print("You win!")
+elif player_choice == "1" and computer_choice == "2":
+    print("You lose.")
+elif player_choice == "1" and computer_choice == "0":
+    print("You win!")
+elif player_choice == "2" and computer_choice == "0":
+    print("You lose")
+elif player_choice == "2" and computer_choice == "1":
+    print("You win!")
