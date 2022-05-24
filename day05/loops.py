d12f7c74c61006b950f10d7ec66b7559a40e6af0
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-24 08:17:32 -0700

    loops and the range function

--- day05/loops.py
+++ day05/loops.py
@@ -5,6 +5,12 @@ for fruit in fruits:
     print(f"{fruits} pie")
     print(fruits)
 
-#For loop with range
+# For loop with range
 # for number in range(1, 11, 3):
 #   print(number)
+
+# add all numbers from 1 to 100 together
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
