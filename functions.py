014b56a9df1a2d9dd94ffd6f5c062b242478b4a6
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-25 23:48:14 -0700

    create first function

---
+++ functions.py
@@ -0,0 +1,15 @@
+# built in functions
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# defining a function
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+# calling a function
+my_function()
