755a790724b58bb88232315b769d06ade39b59f1
CBBell99 <chrisbeckertonbell@gmail.com>
2023-08-07 11:51:36 -0700

    print to console

--- day01/print-to-console.py
+++ day01/print-to-console.py
@@ -1,5 +1,3 @@
-print(
-    "Day 1 - Python Print Function\n"
-    "The function is declared like this:\n"
-    "print('what to print')"
-)
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
